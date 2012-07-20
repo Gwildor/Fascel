2ced315c8c0da9add1af8f7a9d15eede6318b908
Gwildor Sok <gwildorsok@gmail.com>
2012-07-21 00:33:28 +0200

    Initial commit.

---
+++ Fascel/add_release.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'includes.php';
+
+if (isset($_POST['submit'])) {
+
+	// actual code
+
+	?>
+		<div id="fascel_release_added_msg">Release has been added.</div>
+	<?php
+}
+
+?>
+
+<form id="fascel_add_release_form">
+
+</form>
+
+<?php
+
+?>
