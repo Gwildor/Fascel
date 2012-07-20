22e6bdff3c007d4e4504404c28587c7992c5c853
Gwildor Sok <gwildorsok@gmail.com>
2012-07-21 01:00:36 +0200

    Added some html in add_release.php.

--- Fascel/add_release.php
+++ Fascel/add_release.php
@@ -13,7 +13,39 @@ if (isset($_POST['submit'])) {
 
 ?>
 
-<form id="fascel_add_release_form">
+<form id="fascel_add_release_form" method="post">
+
+	<div id="fascel_add_release_version_number">
+		<div>Version number:</div>
+		<div><input type="text" name="version" placeholder="e.g.: 1.0rc2" /></div>
+	</div>
+
+	<div id="fascel_add_release_codename">
+		<div>Codename:</div>
+		<div><input type="text" name="codename" placeholder="optional" /></div>
+	</div>
+
+	<div id="fascel_add_release_datetime">
+		<div>Release date and time:</div>
+		<div><input type="datetime" name="datetime" /></div>
+	</div>
+
+	<div id="fascel_add_release_added">
+		<div>Added:</div>
+		<div><textarea name="added" placeholder="one per line"></textarea></div>
+	</div>
+
+	<div id="fascel_add_release_changed">
+		<div>Changed:</div>
+		<div><textarea name="changed" placeholder="one per line"></textarea></div>
+	</div>
+
+	<div id="fascel_add_release_fixed">
+		<div>Fixed:</div>
+		<div><textarea name="Fixed" placeholder="one per line"></textarea></div>
+	</div>
+
+	<input type="submit" name="submit" value="Add" />
 
 </form>
 
