7b178bafb64808e881fb2baec34770a8e782a320
Gwildor Sok <gwildorsok@gmail.com>
2012-11-17 22:25:11 +0100

    Add loading of the optional dependancies.

--- Fascel/includes.php
+++ Fascel/includes.php
@@ -42,4 +42,50 @@ if (!isset($Fascel['vars']['Fascel_dir'])) {
 $Fascel['vars']['t_re'] = sqlesc($Fascel['config']['table_namespace'].$Fascel['config']['table_names']['releases']);
 $Fascel['vars']['t_ch'] = sqlesc($Fascel['config']['table_namespace'].$Fascel['config']['table_names']['changes']);
 
+
+
+if (is_string($Fascel['config']['jQuery'])) {
+	if ($Fascel['config']['jQuery'][0] != '/' && $Fascel['vars']['Fascel_dir']) {
+		$Fascel['vars']['jQ'] = '../'.$Fascel['config']['jQuery'];
+	} else {
+		$Fascel['vars']['jQ'] = $Fascel['config']['jQuery'];
+	}
+	?>
+	<script type="text/javascript" src="<?php echo $Fascel['vars']['jQ'];?>"></script>
+	<?php
+}
+
+if (is_string($Fascel['config']['jQueryUI'])) {
+	if ($Fascel['config']['jQueryUI'][0] != '/' && $Fascel['vars']['Fascel_dir']) {
+		$Fascel['vars']['jQUI'] = '../'.$Fascel['config']['jQueryUI'];
+	} else {
+		$Fascel['vars']['jQUI'] = $Fascel['config']['jQueryUI'];
+	}
+	?>
+	<script type="text/javascript" src="<?php echo $Fascel['vars']['jQUI'];?>"></script>
+	<?php
+}
+
+if (is_string($Fascel['config']['jQueryUICSS'])) {
+	if ($Fascel['config']['jQueryUICSS'][0] != '/' && $Fascel['vars']['Fascel_dir']) {
+		$Fascel['vars']['jQUICSS'] = '../'.$Fascel['config']['jQueryUICSS'];
+	} else {
+		$Fascel['vars']['jQUICSS'] = $Fascel['config']['jQueryUICSS'];
+	}
+	?>
+	<link rel="stylesheet" type="text/css" href="<?php echo $Fascel['vars']['jQUICSS'];?>" />
+	<?php
+}
+
+if (is_string($Fascel['config']['jQueryTimepickerAddon'])) {
+	if ($Fascel['config']['jQueryTimepickerAddon'][0] != '/' && $Fascel['vars']['Fascel_dir']) {
+		$Fascel['vars']['jQUITA'] = '../'.$Fascel['config']['jQueryTimepickerAddon'];
+	} else {
+		$Fascel['vars']['jQUITA'] = $Fascel['config']['jQueryTimepickerAddon'];
+	}
+	?>
+	<script type="text/javascript" src="<?php echo $Fascel['vars']['jQUITA'];?>"></script>
+	<?php
+}
+
 ?>
